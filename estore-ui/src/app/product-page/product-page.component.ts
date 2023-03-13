b21a420187440fb3e501a4925f9e468b50353921
Domenic Mangano <dm9965@rit.edu>
2023-03-13 14:34:23 -0400

    Overall Webapp Styling Update

--- estore-ui/src/app/product-page/product-page.component.ts
+++ estore-ui/src/app/product-page/product-page.component.ts
@@ -8,6 +8,15 @@ import { Shoe } from "../Shoe";
   styleUrls: ['./product-page.component.scss']
 })
 export class ProductPageComponent {
+	/*@ViewChild('productInfoTemplate', {static: true})
+	productInfoTemplate!: TemplateRef<any>;
+	shoeData: Shoe[] = [];
+	private shoeURL = 'http://localhost:8080/shoes';
+	constructor(private http: HttpClient){}
 
-
+	ngOnInit() {
+		this.http.get<Shoe[]>(this.shoeURL).subscribe(data => {
+			this.shoeData = data;
+		});
+	}*/
 }
